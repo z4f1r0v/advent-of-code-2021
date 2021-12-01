8679915d9378fb2099719d08f74d619f1c7fb39e
z4f1r0v <alexander@zafirov.me>
2021-12-01 22:22:58 +0100

    Simplification

--- src/main/scala/day1/Day1.scala
+++ src/main/scala/day1/Day1.scala
@@ -5,30 +5,19 @@ import os.pwd
 import scala.io.Source
 
 @main def myApp =
-  val input = os.read(pwd / "src" / "main" / "scala" / "day1" / "input")
-    .split("\n")
+  val measurements = os.read(pwd / "src" / "main" / "scala" / "day1" / "input")
+    .linesIterator
+    .map(_.toInt)
+    .toList
 
-  val part1 =
-    input.sliding(2)
-      .foldLeft(0) { (acc, as) =>
-        if as(0).toInt < as(1).toInt
-        then acc + 1
-        else acc
-      }
+  def part1(input: List[Int]) = input.sliding(2).count(s => s.head < s.last)
 
-  println(part1)
+  println(part1(measurements))
 
-  val part2 = input.sliding(3)
-    .sliding(2)
-    .foldLeft(0) { (acc, as) =>
-      def sumElements(group: Array[String]): Int = group.map(_.toInt).sum
+  def part2(input: List[Int]) =
+    val sums = input.sliding(3).map(_.sum)
+    sums.sliding(2).count(s => s.head < s.last)
 
-      //noinspection ZeroIndexToHead
-      if sumElements(as(0)) < sumElements(as(1))
-      then acc + 1
-      else acc
-    }
-
-  println(part2)
+  println(part2(measurements))
 
 
