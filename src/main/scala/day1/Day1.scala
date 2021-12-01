0ef8aaaeca010306a215f0dca26aec8c1603e740
z4f1r0v <alexander@zafirov.me>
2021-12-01 21:52:08 +0100

    Day 1, Second puzzle

--- src/main/scala/day1/Day1.scala
+++ src/main/scala/day1/Day1.scala
@@ -5,14 +5,30 @@ import os.pwd
 import scala.io.Source
 
 @main def myApp =
-  val result = os.read(pwd / "src" / "main" / "scala" / "day1" / "input")
+  val input = os.read(pwd / "src" / "main" / "scala" / "day1" / "input")
     .split("\n")
+
+  val part1 =
+    input.sliding(2)
+      .foldLeft(0) { (acc, as) =>
+        if as(0).toInt < as(1).toInt
+        then acc + 1
+        else acc
+      }
+
+  println(part1)
+
+  val part2 = input.sliding(3)
     .sliding(2)
     .foldLeft(0) { (acc, as) =>
-      if as(0).toInt < as(1).toInt
+      def sumElements(group: Array[String]): Int = group.map(_.toInt).sum
+
+      //noinspection ZeroIndexToHead
+      if sumElements(as(0)) < sumElements(as(1))
       then acc + 1
       else acc
     }
-  println(result)
+
+  println(part2)
 
 
