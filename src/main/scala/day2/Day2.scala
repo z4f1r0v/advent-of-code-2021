4c758c48b4b3a70fbacbba60b0c4f8502291ca53
z4f1r0v <alexander@zafirov.me>
2021-12-02 20:22:28 +0100

    Day 2, Second puzzle

--- src/main/scala/day2/Day2.scala
+++ src/main/scala/day2/Day2.scala
@@ -1,24 +1,45 @@
 package day2
 
+import day2.Position
 import util.FileUtil
 
-@main def myApp =
+@main def myApp2 =
   val commands = FileUtil.getLinesIterator(adventDay = 2).toList
 
+  def submarineNavigation(input: List[String], commandInterpreter: (Position, String, Int) => Position): Int = {
+    input.foldLeft(Position()) {
+      (p, c) =>
+        val commandUnit = c.split(" ")
+        val command = commandUnit(0)
+        val unit = commandUnit(1).toInt
+        commandInterpreter(p, command, unit)
+    }.product
+  }
+
   def part1(input: List[String]) =
-    input.foldLeft(Position()) { (p, c) =>
-      val commandValue = c.split(" ")
-      val command = commandValue(0)
-      val value = commandValue(1).toInt
-      command match {
+    submarineNavigation(
+      input,
+      (p, command, value) => command match {
         case "forward" => p.copy(horizontal = p.horizontal + value)
         case "down" => p.copy(depth = p.depth + value)
         case "up" => p.copy(depth = p.depth - value)
       }
-    }.product
+    )
 
   println(part1(commands))
 
-private case class Position(horizontal: Int = 0, depth: Int = 0) {
+  def part2(input: List[String]) =
+    submarineNavigation(
+      input,
+      (p, command, unit) => command match {
+        case "forward" => p.copy(horizontal = p.horizontal + unit, depth = p.depth + p.aim * unit)
+        case "down" => p.copy(aim = p.aim + unit)
+        case "up" => p.copy(aim = p.aim - unit)
+      }
+    )
+
+  println(part2(commands))
+
+private case class Position(horizontal: Int = 0, depth: Int = 0, aim: Int = 0) {
   def product: Int = horizontal * depth
 }
