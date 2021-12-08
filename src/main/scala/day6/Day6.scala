7bce3063e1ef08d380a2b516e9be293c91045d46
z4f1r0v <alexander@zafirov.me>
2021-12-08 14:50:52 +0100

    Day 6

---
+++ src/main/scala/day6/Day6.scala
@@ -0,0 +1,23 @@
+package day6
+
+import util.FileUtil
+
+// https://adventofcode.com/2021/day/6
+@main def myApp6 =
+
+  def input2state(input: Seq[Int]): Vector[Long] = Vector.tabulate(9)(i => input.count(_ == i))
+
+  def stepState(state: Vector[Long]): Vector[Long] = {
+    val zeroCount +: stateNonZero = state
+    stateNonZero.updated(6, stateNonZero(6) + zeroCount) :+ zeroCount
+  }
+
+  def countFish(input: List[Int])(after: Int): Long =
+    Iterator.iterate(input2state(input))(stepState).drop(after).next().sum
+
+  val input = FileUtil.readInput(6).split(",").map(_.toInt).toList
+
+  val countWithInput = countFish(input)
+
+  println(countWithInput(80))
+  println(countWithInput(256))
